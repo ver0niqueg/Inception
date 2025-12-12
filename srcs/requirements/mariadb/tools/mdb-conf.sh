8d120a4957636a124b3db599ac75108736316bcf
Veronique Galmiche <vgalmich@f4r4s14.clusters.42paris.fr>
2025-12-12 16:41:47 +0100

    repo cleaned

--- srcs/requirements/mariadb/tools/mdb-conf.sh
+++ srcs/requirements/mariadb/tools/mdb-conf.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-# Read passwords from Docker secrets
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-# Create socket directory
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
 #--------------mariadb start--------------#
-# start mariadb in background for initial config
 mysqld --user=mysql --datadir=/var/lib/mysql &
 sleep 5 # wait for mariadb to start
 
 #--------------mariadb config--------------#
-# create mariadb if not exists
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
 
-# create user if not exists
 mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-# give privileges to user
 mariadb -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DB}\`.* TO \`${MYSQL_USER}\`@'%';"
 
-# flush privileges to apply changes
 mariadb -e "FLUSH PRIVILEGES;"
 
 #--------------mariadb restart---------------#
-# shutdown mariadb to restart with new config
 mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
 
-# restart mariadb with new config in the foreground to keep the container running
 mysqld_safe
--- srcs/requirements/wordpress/tools/wp_conf.sh
+++ srcs/requirements/wordpress/tools/wp_conf.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
 
-# Read passwords from Docker secrets
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
 #--------------------wp installation--------------------#
-# wp-cli installation
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-# wp-cli permission
+
 chmod +x wp-cli.phar
-# wp-cli move to bin
+
 mv wp-cli.phar /usr/local/bin/wp
 
-# go to wordpress directory
 cd /var/www/wordpress
-# give permission to wordpress directory
-chmod -R 755 /var/www/wordpress
-# change owner of wordpress directory to www-data
-chown -R www-data:www-data /var/www/wordpress
-#--------------------wp installation--------------------#
 
-# download wordpress core files
+chmod -R 755 /var/www/wordpress
+
+chown -R www-data:www-data /var/www/wordpress
+
+#--------------------wp installation--------------------#
 wp core download --allow-root
-# create wp-config.php file with database details
+
 wp config create --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
-# install wordpress with the giver title, admin username, password and email
+
 wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
-# create a new user with the given username, email, password and role
+
 wp user create "$WP_USER" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --role=editor --allow-root
 
 #--------------------wp config--------------------#
-# change listen port from unix socket to 9000
 sed -i 's@/run/php/php8.2-fpm.sock@9000@' /etc/php/8.2/fpm/pool.d/www.conf
-# create a directory for php-fpm
+
 mkdir -p /run/php
-# start php-fpm service in the foreground to keep the container running
+
 /usr/sbin/php-fpm8.2 -F
